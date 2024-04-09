5f86788bac577599beed92658105cacd748d2fc3
Felipe Kucharski de Barbosa <felipekb2000@gmail.com>
2024-04-09 02:02:16 -0300

    ajuste key final

--- src/app/actions/getPrices.action.ts
+++ src/app/actions/getPrices.action.ts
@@ -1,12 +1,13 @@
 "use server";
 
 export async function getPrice() {
+  const headers = new Headers();
+  headers.append("X-CMC_PRO_API_KEY", process.env.API_KEY || "");
+
   const response = await fetch(
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=100",
     {
-      headers: {
-        "X-CMC_PRO_API_KEY": process.env.API_KEY || "",
-      },
+      headers: headers,
     },
   );
   const data = response.json();
